a2caff78a9bc9654ecd0348dde312e17deebaff3
samira <samira.barbar@yahoo.com>
2018-07-10 23:47:08 -0700

    const port is add

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express=require('express');
 const app=express();
+const port=3000;
  app.get('/',(req,res)=>{
      res.send("hello");
  });
- app.listen(3000,()=>
+ app.listen(port,()=>
 {
-    console.log("server is running port 3000");
+    console.log(`server is running on port ${port}`);
 })
